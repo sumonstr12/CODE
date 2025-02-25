a23b01db8c93c8fe3b1f00389d719572377b4883
sumonstr12 <sumon220212@cseku.ac.bd>
2025-02-25 20:29:14 +0600

    Update file add new solve

---
+++ Problem_Solution_Online/CodeForces/Contest-25/AUST Contest-25/I.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve(){
+    int n;
+    cin>> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());   
+    
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t; // number of test cases
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    
+}
